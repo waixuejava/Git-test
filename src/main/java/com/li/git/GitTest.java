3d90571e6eb6cabe4e4d199353226bad51895a33
li <1663315684@qq.com>
2022-07-04 00:46:19 +0800

    first commit

---
+++ src/main/java/com/li/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.li.git;
+
+/**
+ * @Author LI
+ * @create 2022/7/4 0:39
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
