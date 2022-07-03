33eefbd9b4a717dbdf8a1060217ed7b6b3dfb206
li <1663315684@qq.com>
2022-07-04 00:52:41 +0800

    second commit

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -7,5 +7,6 @@ package com.li.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
     }
 }
