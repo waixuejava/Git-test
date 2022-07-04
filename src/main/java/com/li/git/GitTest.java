6c4b711ada9e14ccb2e344ef20070f6f0e2e4db6
waixuejava <103633283+waixuejava@users.noreply.github.com>
2022-07-04 22:23:29 +0800

    liwenchaun

--- src/main/java/com/li/git/GitTest.java
+++ src/main/java/com/li/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello Git5");
         System.out.println("hello Git6");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
